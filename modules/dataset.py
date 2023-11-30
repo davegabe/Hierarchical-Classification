93eac4fab4aeb1132837481421f369ee98f891ca
Davide <me@davegabe.it>
2023-11-30 11:10:11 +0100

    Progress in the hierarchy tree builder

--- modules/dataset.py
+++ modules/dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 from nltk.corpus import wordnet as wn
 import nltk
+from nltk.corpus.reader.wordnet import Synset
 from torchvision.transforms import transforms
 import pandas as pd
 import numpy as np
@@ -71,30 +72,84 @@ class HierarchicalImageNet(Dataset):
         """
         # Read synset ids for classes
         map_fname = os.path.join(self.root, "LOC_synset_mapping.txt")
-        synset_ids = []
+        synsets = []
         with open(map_fname, 'r') as f:
             for line in f:
                 synset_id_s = line.split()[0]  # e.g. n01440764
                 if synset_id_s not in self.classes:
                     continue
                 synset_id = int(synset_id_s[1:])  # e.g. 01440764
-                synset_ids.append(synset_id)
-        synset_ids = np.array(synset_ids)
+                synset: Synset = wn.synset_from_pos_and_offset('n', synset_id)
+                synsets.append(synset)
 
-        # Create the list of parents for all classes
-        all_list = [
-            ["-" for j in range(self.max_hierarchy_depth)]
-            for i in range(self.n_classes)
-        ]
-        for i in range(self.n_classes):
-            synset = wn.synset_from_pos_and_offset('n', int(synset_ids[i]))
-            hyper_list = [synset.name()]
+        # Initialize the nodes
+        nodes = {}
+        for synset in synsets:
+            # Get the hypernyms of the synset
+            nodes[synset.name()] = {'children': [], 'parent': None}
+
+        # Create the tree from the synset ids
+        for synset in synsets:
+            # For each hypernym of the synset (and for each hypernym of hypernyms)
             while synset.hypernyms():
-                synset = synset.hypernyms()[0]
-                hyper_list.append(synset.name())
-            hyper_list.insert(0, 'null')
-            hyper_list.insert(0, 'null')
-            all_list[i][:] = hyper_list[self.max_hierarchy_depth:-1]
+                # Get the hypernyms of the synset
+                hypernym = synset.hypernyms()[0]
+                name = hypernym.name()
+                nodes[synset.name()]['parent'] = name
+                if name not in nodes:
+                    nodes[name] = {'children': [synset.name()], 'parent': None}
+                if synset.name() not in nodes[name]['children']:
+                    nodes[name]['children'].append(synset.name())
+                # Set the synset to the hypernym
+                synset = hypernym
+
+        # Collapse nodes with only one child into their child
+        while True:
+            changed = False
+            # For each leaf node
+            for synset in synsets:
+                parent = nodes[synset.name()]['parent']
+                if parent is None:
+                    raise ValueError(f"Node {synset.name()} has no parent")
+                # If the node has only one child
+                if len(nodes[parent]['children']) == 1:
+                    # The only child of the parent is synset.name()
+                    # Remove the parent from the tree
+                    grandparent = nodes[parent]['parent']
+                    nodes[grandparent]['children'].remove(parent)
+                    nodes[grandparent]['children'].append(synset.name())
+                    nodes[synset.name()]['parent'] = grandparent
+                    # Remove the parent from the list of nodes
+                    del nodes[parent]
+                    changed = True
+            if not changed:
+                break
+
+        # Find the root of the tree
+        root = None
+        for name, node in nodes.items():
+            if node['parent'] is None:
+                root = name
+                break
+        # Find the first node with multiple children (common root)
+        while len(nodes[root]['children']) == 1:
+            root = nodes[root]['children'][0]
+
+        # Compute the depth of each node
+        max_depth = 0
+        for synset in synsets:
+            depth = 0
+            name = synset.name()
+            while name != root:
+                name = nodes[name]['parent']
+                depth += 1
+            if depth > max_depth:
+                max_depth = depth
+        print(f"Max depth: {max_depth}")
+
+        # Create the hierarchy dataframe starting from the root
+        all_list = []
+        # TODO: Create the hierarchy dataframe starting from the root
 
         # Create the dataframe
         df = pd.DataFrame(all_list)
