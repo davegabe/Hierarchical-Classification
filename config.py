8c1229b4a8f61969cfaf8410a2a0986633da77e7
Riccardo <github@dickon.it>
2024-02-17 15:07:22 +0100

    maintenance

--- config.py
+++ config.py
@@ -5,8 +5,10 @@ IMAGE_SIZE = (128, 128)
 MODEL_NAME = "vgg16"  # "vgg16" or "branch_vgg16"
 BRANCH_SELECTOR = "learnable"  # "static" or "learnable"
 N_BRANCHES = 3
-LEARNING_RATE = 0.0001
+LEARNING_RATE = 3.311311214825912e-05
 L1_REGULARIZATION = 1e-5
+LOG_STEP = 1
+PRIVILEGED = False
 SIMILARITY_REGULARIZATION = 0.001
 NUM_EPOCHS = 50
 VAL_EPOCHS = 5
@@ -16,3 +18,5 @@ LIMIT_IMAGES = 500 # limit the number of images per class, -1 means no limit
 
 TRAIN_DATASET_PATH = "dataset/train"
 VAL_DATASET_PATH = "dataset/val"
+
+USE_WANDB = False
